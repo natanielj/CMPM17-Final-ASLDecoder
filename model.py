597d5b28e6a173e671d50674b04d33b85f1c6d5d
duashmita <duashmita@gmail.com>
2025-03-13 18:32:58 -0700

    transfor errors,(old, yay.)

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from torchvision import transforms
+from torchvision.transforms import v2
 from torchvision.io import read_image
 import torch.nn as nn
 import torch.optim as optim
@@ -6,9 +6,10 @@ from torch.utils.data import DataLoader, Dataset
 import torch
 import os
 import random
-
+from PIL import Image
+from torchvision.transforms.functional import to_pil_image
 # Directory for dataset
-dir_path = '/asl_alphabet_train'
+dir_path = './asl_alphabet_train'
 
 # Class for dataset
 class ASLDataset(Dataset):
@@ -16,7 +17,7 @@ class ASLDataset(Dataset):
 
         # Basic Definition
         self.root_dir = root_dir
-        self.transform = transform
+        #self.transform = transform
 
         # More details on how this code works in engineering notebook
         # Get's the list of all the folders under the main directory
@@ -39,24 +40,24 @@ class ASLDataset(Dataset):
                     self.char.append(class_name)
 
         for i in range(0,len(self.image_paths)):
-
             if random.choice([True, False, False, False, False, False, False, False]):
-
             #Data Augmentation function definition
-                original_img = read_image(self.image_paths[i]).float() / 255.0
-                augment_transform = transforms.Compose([
-                    transforms.Resize((64, 64)),
-                    transforms.RandomHorizontalFlip(p=0.5),
-                    transforms.RandomVerticalFlip(p=0.5),
+                original_img = Image.open(self.image_paths[i])
+                trip = v2.Compose([
+                    # transforms.Resize(200, 200),
+                    v2.RandomHorizontalFlip(p=0.5),
+                    v2.RandomVerticalFlip(p=0.5),
+                    v2.ToImage(),
+                    v2.ToDtype(torch.float32, scale=True),
+                    v2.Normalize(mean=[0.485, 0.465, 0.406], std=[0.229, 0.224, 0.225])
                 ])
 
                 # Convert tensor to PIL image for saving
-                to_pil = transforms.ToPILImage()
                 for j in range(5):
-                    augmented = augment_transform(original_img)
-                    augmented_img = to_pil(augmented)
-                    augmeneted_img_path = f"Dataset/asl_alphabet_train/{self.char[i]}/DA_{j}.jpg"
-                    augmented_img.save(augmeneted_img_path)
+                    aug = trip(original_img)
+                    augmented = to_pil_image(aug)
+                    augmeneted_img_path = f"asl_alphabet_train/{self.char[i]}/DA_{j}.jpg"
+                    augmented.save(augmeneted_img_path)
                     self.image_paths.append(augmeneted_img_path)
                     self.labels.append(self.class_to_idx[self.char[i]])
                     self.char.append(self.char[i])
@@ -66,24 +67,30 @@ class ASLDataset(Dataset):
 
     def __getitem__(self, idx):
         # Read image as tensor and normalize to [0, 1]
-        image = read_image(self.image_paths[idx]).float() / 255.0
-        if self.transform:
-            image = self.transform(image)
+
+        image = self.image_paths[idx]
         label = self.labels[idx]
         return image, label
 
 # Class for CNN model defination
 class ASLCNN(nn.Module):
     def __init__(self, num_classes, input_channels=3):
+        
         super(ASLCNN, self).__init__()
+
+        b2tf = v2.Compose([
+            v2.ToTensor()
+        ])
+
+        input_channels = b2tf(input_channels)
         self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(256 * 4 * 4, 64)
-        self.fc2 = nn.Linear(64, num_classes)
+        self.fc1 = nn.Linear(256 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
@@ -101,12 +108,12 @@ batch_size = 32
 learning_rate = 0.001
 
 # Model instanciation
-dataset = ASLDataset(root_dir=dir_path, transform=transforms)
+dataset = ASLDataset(root_dir=dir_path, transform=v2)
 train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 num_classes = len(dataset.classes)
 model = ASLCNN(num_classes=num_classes)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -116,15 +123,13 @@ for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
     for images, labels in train_loader:
-        images, labels = images.to(device), labels.to(device)
+        # images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         loss = criterion(model(images), labels)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
     print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}")
-
-
 
 #Filtering testing data
 images = os.listdir("/asl_alphabet_test")
@@ -136,7 +141,7 @@ for i in images:
 model.eval()
 correct, total = 0, 0
 with torch.no_grad():
-    images, labels = images.to(device), labels.to(device)
+    # images, labels = images.to(device), labels.to(device)
     outputs = model(images)
     _, predicted = torch.max(outputs, 1)
     total += labels.size(0)
