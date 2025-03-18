7a9f0171a0054b1c49dedd88ce603f8715c1f8b2
Nataniel Jayaseelan <63678990+natanielj@users.noreply.github.com>
2025-03-18 06:51:57 -0700

    camera and ui works

--- gui/app.py
+++ gui/app.py
@@ -1,64 +1,75 @@
-from PyQt6.QtWidgets import * 
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
-import sys 
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QThread
+import sys
 from os import remove
 from camera import CameraApp
 
-class startWindow(QWidget):
+class StartWindow(QWidget):
     def __init__(self, parent=None):
-        super(startWindow, self).__init__(parent)
+        super(StartWindow, self).__init__(parent)
         self.setWindowTitle("Simple ASL Decoder - Start")
         self.setGeometry(500, 200, 750, 750)
 
-        camera_widget = CameraApp()
-        camera_widget.graphicsView.setParent(self)
-        camera_widget.graphicsView.setGeometry(125, 100, 500, 300)
+        # Create and set up the camera widget.
+        self.camera_widget = CameraApp()
+        self.camera_widget.graphicsView.setParent(self)
+        self.camera_widget.graphicsView.setGeometry(125, 100, 500, 300)
 
-        readButton = QPushButton("Read Sign!", self)
+        # Create a button to trigger reading the sign.
+        self.readButton = QPushButton("Read Sign!", self)
+        self.readButton.setGeometry(300, 500, 150, 50)  # x, y, width, height
 
-        readButton.setGeometry(300, 500, 150, 50) # x, y, width, height
 
-
-class ResultsWindow(QWidget): 
-    def __init__(self, parent=None): 
+class ResultsWindow(QWidget):
+    def __init__(self, parent=None):
         super(ResultsWindow, self).__init__(parent)
-        
-        label = QLabel(self)
+        # Use a layout to add the label (setCentralWidget is not available in QWidget)
+        layout = QVBoxLayout(self)
+        self.label = QLabel(self)
         pixmap = QPixmap('temp_image_in.png')
-        label.setPixmap(pixmap)
-        self.setCentralWidget(label)
+        self.label.setPixmap(pixmap)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
+    def updatePhoto(self, filename="temp_image_in.png"):
+        pixmap = QPixmap(filename)
+        self.label.setPixmap(pixmap)
 
-class MainApp(QMainWindow): 
-    def __init__(self): 
-        super(MainApp, self).__init__() 
+class MainApp(QMainWindow):
+    def __init__(self):
+        super(MainApp, self).__init__()
         self.setWindowTitle("Simple ASL Decoder")
 
+        # Create a stacked widget and set it as the central widget.
         self.stack = QStackedWidget(self)
-        self.startStack = QWidget()
-        self.resultStack = QWidget()
         self.setCentralWidget(self.stack)
 
-        self.start = startWindow()
+        # Create instances of StartWindow and ResultsWindow.
+        self.start = StartWindow()
         self.result = ResultsWindow()
 
-        self.stack.addWidget(self.startStack)
-        self.stack.addWidget(self.resultStack)
+        # Add the windows to the stack.
+        self.stack.addWidget(self.start)
+        self.stack.addWidget(self.result)
 
-        self.startWindow.readButton.clicked.connect(self.result)
+        # Connect the read button signal to switch to the results view.
+        self.start.readButton.clicked.connect(self.takePhotoAndShowResults)
         
-
-    def showResults(self):
-        self.stack.setCurrentWidget(self.start)
+    def takePhotoAndShowResults(self):
+        # Capture the photo using the CameraApp's capturePhoto method.
+        self.start.camera_widget.capturePhoto()
+        # Update the results window with the captured image.
+        self.result.updatePhoto()
+        # Switch to the results view.
+        self.stack.setCurrentWidget(self.result)
 
     def closeEvent(self, event):
-        # Handle the close event
         print("Closing the application...")
         remove('temp_image_in.png')
         event.accept()
 
-
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainApp()
